5f757af1066ba73c148db93708583887832943d5
MarceloAtencio <atencio.marcelo@hotmail.com>
2023-12-14 22:09:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ app = FastAPI(title='Proyecto Individual ',
 
 
 @app.get("/genero/{x}",tags=["Género"])
-def PlayTimeGenre(genero):
+def PlayTimeGenre(x: str):
     # Cargar el archivo CSV en un DataFrame
     df = pd.read_csv("../PI 1/ETL/03 - Dataframe para funciones/PlayTimeGenre.csv")
 
     # Filtrar el DataFrame para el género específico
-    df_genero = df[df['Genero'] == genero]
+    df_genero = df[df['Genero'] == x]
 
     # Encontrar el año con más horas jugadas para ese género
     año_max_playtime = df_genero.loc[df_genero['playtime_forever'].idxmax(), 'Año_Lanzamiento']
 
     # Crear la leyenda
-    leyenda = f"Año de lanzamiento con más horas jugadas para Género {genero}: {año_max_playtime}"
+    leyenda = f"Año de lanzamiento con más horas jugadas para Género {x}: {año_max_playtime}"
 
     return leyenda
